f5fcf6a041c41c6362cd887af7b80e78b4c67bd3
BernardoFOFG <bernardofofg@gmail.com>
2024-05-02 10:02:01 -0300

    :recycle: refactor: calling markdown files correctly

--- src/routes/america/+page.svelte
+++ src/routes/america/+page.svelte
@@ -1,43 +1,8 @@
 <script lang="ts">
+  import { america } from "$lib";
   import Form from "$lib/components/Form/Form.svelte";
   import Markdown from "$lib/components/Markdown/Markdown.svelte";
   import Text from "$lib/components/Text/Text.svelte";
-
-  let markdownText = `
-  # Descobrimento da América
-
-## Introdução
-
-O descobrimento da América é um dos eventos mais significativos da história, marcando o encontro de dois mundos distintos e o início de uma nova era de exploração e intercâmbio cultural.
-
-## Cristóvão Colombo
-
-Em 1492, o navegador genovês Cristóvão Colombo, financiado pelos Reis Católicos da Espanha, embarcou em uma jornada ousada em busca de uma rota marítima para as Índias. Acreditava-se que tal rota poderia ser encontrada navegando para o oeste, contornando a Terra.
-
-### A Viagem
-
-Colombo partiu com três navios: a Santa Maria, a Pinta e a Niña. Após semanas de viagem, em 12 de outubro de 1492, a tripulação avistou terra, uma ilha nas Bahamas, inaugurando um novo capítulo na história mundial.
-
-## O Encontro de Dois Mundos
-
-O descobrimento da América marcou o encontro entre o Velho Mundo e o Novo Mundo. Este evento teve impactos profundos na geografia, cultura, comércio e sociedade de ambas as partes.
-
-### Trocas Culturais
-
-A troca colombiana, como ficou conhecida, envolveu a transferência de plantas, animais, tecnologias e culturas entre o hemisfério oriental e o ocidental. Isso teve consequências duradouras, impactando a alimentação, medicina, e estilos de vida em ambos os continentes.
-
-## Impactos Históricos
-
-O descobrimento da América não apenas alterou as rotas comerciais globais, mas também teve implicações políticas e sociais. O estabelecimento de colônias europeias nas Américas resultou em conflitos, colonização e interações complexas entre diferentes povos.
-
-### Legado
-
-O legado do descobrimento da América é multifacetado, com aspectos positivos e negativos. Enquanto trouxe avanços tecnológicos, também desencadeou séculos de exploração, conquista e mudanças profundas nas sociedades indígenas.
-
-# Conclusão
-
-O descobrimento da América é um marco que ressoa ao longo da história, refletindo não apenas a busca por novas rotas comerciais, mas também o encontro e interação de diferentes civilizações. Este evento moldou o mundo que conhecemos hoje, impactando culturas, economias e sociedades de maneiras complexas e duradouras.
-  `;
 
   let valueInput: string;
 
@@ -55,10 +20,9 @@ O descobrimento da América é um marco que ressoa ao longo da história, reflet
 <Form router="america" onInputValueChange={handleInputValueChange}>
   {#if valueInput}
     <p>
-      {(markdownText.match(new RegExp(valueInput, "gi")) || []).length} ocorrências
-      encontradas.
+      {(america.match(new RegExp(valueInput, "gi")) || []).length} ocorrências encontradas.
     </p>
   {/if}
 </Form>
 
-<Markdown label={markdownText} />
+<Markdown label={america} />
